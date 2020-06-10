2af5c0d1ea74c4d8811087cf8012efae9b7d054f
cassandra delieto <cassandradelieto@gmail.com>
2020-06-09 22:39:36 -0700

    user details link on homepage fixed

--- crud.py
+++ crud.py
@@ -28,6 +28,12 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
@@ -37,6 +43,24 @@ def create_rating(user, movie, score):
     db.session.commit()
 
     return rating
+
+
+def get_movie_by_id(movie_id):
+    """Return a movie via key"""
+
+    return Movie.query.get(movie_id)
+
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()    
+
+def get_user_by_id(user_id):
+    """Return a user by id."""
+
+    return User.query.filter(User.user_id == user_id).first()      
+
 
 if __name__ == '__main__':
     from server import app
